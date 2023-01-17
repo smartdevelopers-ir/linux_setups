ac042001ad4acc19181ebf24795fd18f0d3b7fe1
smartdevelopers-ir <64631690+smartdevelopers-ir@users.noreply.github.com>
2023-01-17 23:19:39 +0330

    Add files via upload

---
+++ ssh_login_watcher.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+while inotifywait -e modify /var/log/auth.log
+do
+	if tail -n1 /var/log/auth.log | grep dropbear | grep "Password auth succeeded for"
+	then
+		bash /usr/local/bin/ssh_session_check.sh
+	fi
+done
